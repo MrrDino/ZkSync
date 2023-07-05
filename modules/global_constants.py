a190215340bb5701a7dd5791c63cb32870ba05dd
dino <kropachevsemen15@gmail.com>
2023-07-05 19:50:38 +0300

    Add ready SyncSwap

--- modules/global_constants.py
+++ modules/global_constants.py
@@ -7,3 +7,5 @@ TIMEOUT = 10
 # SWAP SIZE
 MIN_AMOUNT = 0.0040783968  # ETH
 MAX_AMOUNT = 0.007646994  # ETH
+
+SWAP_BACK = True  # Нужно ли менять актив обратно
--- modules/syncswap/syncswap.py
+++ modules/syncswap/syncswap.py
@@ -7,15 +7,17 @@ import eth_abi
 import constants as cst
 
 from web3 import Web3
-from modules.helper import pre_check
-from abis.erc20 import ERC20_ABI
-from abis.router import ROUTER_ABI
-from abis.factory import FACTORY_ABI
+
+from hexbytes.main import HexBytes
 from web3.types import TxParams, ChecksumAddress
 from eth_account.signers.local import LocalAccount
 from web3.middleware import construct_sign_and_send_raw_middleware
 
-from modules.global_constants import TIMEOUT, MIN_AMOUNT, MAX_AMOUNT
+from abis.pool import POOL_ABI
+from abis.erc20 import ERC20_ABI
+from abis.router import ROUTER_ABI
+from abis.factory import FACTORY_ABI
+from modules.global_constants import MIN_AMOUNT, MAX_AMOUNT
 
 
 class SyncSwap:
@@ -23,68 +25,66 @@ class SyncSwap:
     def __init__(self):
         pass
 
-    def start(self):
-
-        check = False
-
-        while not check:
-
-            check, keys, proxies = pre_check()
-
-            if not check:
-                time.sleep(TIMEOUT)
-
-        for key, proxy in zip(keys, proxies):
-
-            w3 = self.connect()
-            account = self.get_account(w3=w3, key=key)
-            w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
-            amount = int(random.uniform(MIN_AMOUNT, MAX_AMOUNT) * 10 ** 18)
-
-            if not self.check_balance(w3=w3, wallet=account.address, amount=amount):
-                continue
-
-    def check_swap(self, key: str):
+    def start_swap(self, key: str, token0: str, token1: str, amount: float = None) -> int or None:
         """Функция для проверки tokens swap"""
 
         w3 = self.connect()
         account = self.get_account(w3=w3, key=key)
         w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
-        amount = int(0.00252 * 10 ** 18)
 
-        self.make_swap(w3=w3, amount=amount, account=account)
+        if not amount:
+            amount = self.get_amount(w3=w3, wallet=account.address)
+
+            if amount == 0:
+                return False
+
+            success = self.make_swap(w3=w3, amount=amount, account=account, token0=token0, token1=token1)
+            return amount
+        else:
+            success = self.make_swap(w3=w3, amount=amount, account=account, token0=token0, token1=token1)
 
     def make_swap(
             self,
             w3: Web3,
-            amount: int,
+            amount: int or float,
             account: LocalAccount,
             token0: str = cst.ETH,
             token1: str = cst.USDC
-    ):
+    ) -> bool:
         """Функция выполнения обмена"""
 
         signer = account.address
-        token0 = Web3.to_checksum_address(token0)
-        token1 = Web3.to_checksum_address(token1)
+        token0 = self.to_address(token0)
+        token1 = self.to_address(token1)
+        token_in = cst.TOKENS[token0.lower()]  # если ETH -> поведение меняется)
         router = self.get_contract(w3=w3, address=cst.ROUTER, abi=ROUTER_ABI)
         pool = self.get_contract(w3=w3, address=cst.POOL_FACTORY, abi=FACTORY_ABI)
         pool_address = pool.functions.getPool(token0, token1).call()
 
-        try:
-            approved_tx = self.approve_swap(
-                w3=w3,
-                amount=amount,
-                signer=account,
-                spender=cst.ROUTER,
-                token=token0,
-            )
-            time.sleep(50)
+        #  Если повторный свап -> переводим сумму из ETH в USDC
+        if isinstance(amount, float):
+            rate = self.get_rate(w3=w3, pool=pool_address, token_ch=token0)
+            amount = self.get_swap_amount(amount=amount, rate=rate)
 
-            approve_status = self.check_tx(w3=w3, tx_hex=approved_tx)
-            print(approve_status)
-        except Exception as err:
-            print(err)
+        print(amount)
+
+        if token_in != 'ETH':
+
+            try:
+                approved_tx = self.approve_swap(
+                    w3=w3,
+                    amount=amount,
+                    signer=account,
+                    spender=cst.ROUTER,
+                    token=token0,
+                )
+                tx_rec = w3.eth.wait_for_transaction_receipt(approved_tx)
+                status = tx_rec['status']
+                print(f'Tx: {approved_tx.hex()}. Status: {status}')
+
+                time.sleep(50)
+            except Exception as err:
+                print(err)
 
         steps = [
             {
@@ -98,30 +98,60 @@ class SyncSwap:
             }
         ]
 
-        paths = [{'steps': steps, 'tokenIn': token0, 'amountIn': amount}]
+        paths = [
+            {
+                'steps': steps,
+                'tokenIn': cst.ZERO_ADDRESS if token_in == 'ETH' else token0,
+                'amountIn': amount
+            }
+        ]
 
-        tx = self.create_swap_tx(w3=w3, paths=paths, router=router, wallet=signer)
+        tx = self.create_swap_tx(
+            w3=w3,
+            paths=paths,
+            wallet=signer,
+            router=router,
+            amount=amount,
+            token_in=token_in
+        )
+
+        tx.update(
+            {
+                'gas': w3.eth.estimate_gas(tx),
+                'maxFeePerGas': w3.eth.gas_price,
+                'maxPriorityFeePerGas': w3.eth.gas_price
+            }
+        )
+
         signed_tx = account.sign_transaction(transaction_dict=tx)
+        time.sleep(30)
 
         try:
             swap_tx = w3.eth.send_raw_transaction(transaction=signed_tx.rawTransaction)
             tx_rec = w3.eth.wait_for_transaction_receipt(swap_tx)
-            print(swap_tx.hex())
-            print(tx_rec)
-            time.sleep(50)
+            status = tx_rec['status']  # будет использоваться для переотправки
+            print(f'Tx: {swap_tx.hex()}. Status: {status}')
+
         except BaseException as err:
             print(err)
 
-    def check_balance(self, w3: Web3, wallet: str, amount: float) -> bool:
+        return True
+
+    def get_amount(self, w3: Web3, wallet: str) -> int:
         """Функция проверки баланса"""
 
         wallet = self.to_address(address=wallet)
-        eth_balance = w3.eth.get_balance(wallet) / 10 ** 18
+        eth_balance = w3.eth.get_balance(wallet)
+        min_amount = MIN_AMOUNT * 10 ** 18
+        max_amount = MAX_AMOUNT * 10 ** 18
 
-        if eth_balance < amount:
-            return False
-
-        return True
+        if eth_balance < min_amount:  # простая проверка на баланс
+            return 0
+        elif eth_balance < max_amount:
+            eth_balance = int(eth_balance * .9)
+            return random.randrange(min_amount, eth_balance)
+        else:
+            return random.randrange(min_amount, max_amount)
 
     def get_contract(self, w3: web3.Web3, address: str, abi: list) -> web3.contract.Contract:
         """Функция получения пула"""
@@ -139,7 +169,7 @@ class SyncSwap:
             signer: LocalAccount,
             amount: int,
             w3: Web3
-    ) -> str:
+    ) -> HexBytes:
         """Функция утверждения использования средств"""
 
         token_contract = w3.eth.contract(address=token, abi=ERC20_ABI)
@@ -156,14 +186,18 @@ class SyncSwap:
             })
 
             approve_tx = signer.sign_transaction(transaction)
+            time.sleep(30)
+
             tx = w3.eth.send_raw_transaction(approve_tx.rawTransaction)
 
-            return tx.hex()
+            return tx
 
     @staticmethod
     def create_swap_tx(
             router: web3.contract.Contract,
             wallet: ChecksumAddress,
+            token_in: str,
+            amount: int,
             paths: list,
             w3: Web3,
     ) -> TxParams:
@@ -173,22 +207,37 @@ class SyncSwap:
             0,
             int(time.time()) + 1800,
         ).build_transaction({
+            'gas': 0,
             'from': wallet,
-            'gas': 3_000_000,
-            'gasPrice': w3.eth.gas_price,
+            'maxFeePerGas': 0,
+            'maxPriorityFeePerGas': 0,
+            'value': amount if token_in == 'ETH' else 0,
             'nonce': w3.eth.get_transaction_count(wallet),
         })
 
         return txn
 
+    def get_rate(self, w3: Web3, pool: ChecksumAddress, token_ch: ChecksumAddress) -> float:
+        """Функция получения курса в пуле"""
+
+        contract = self.get_contract(w3=w3, address=pool, abi=POOL_ABI)
+        reserves = contract.functions.getReserves().call()
+        token0 = contract.functions.token0().call()
+
+        if cst.TOKENS[token0.lower()] == 'ETH':
+            usd = int(reserves[1] / 10 ** 6) / int(reserves[0] / 10 ** 18)
+        else:
+            usd = int(reserves[0] / 10 ** 6) / int(reserves[1] / 10 ** 18)
+
+        usd -= usd * cst.MULTS[token_ch.lower()]
+
+        return usd
+
     @staticmethod
-    def check_tx(w3: Web3, tx_hex) -> int:
-        """Функция проверки транзакции"""
+    def get_swap_amount(amount: float, rate: float, dec: int = 6) -> int:
+        """Функция суммы для обмена USDT/USDC"""
 
-        tx = w3.eth.get_transaction_receipt(transaction_hash=tx_hex)
-        print(tx)
-
-        return tx['status']
+        return int(amount * rate * (10 ** dec))
 
     @staticmethod
     def to_address(address: str) -> web3.main.ChecksumAddress or None:
@@ -213,3 +262,4 @@ class SyncSwap:
 
         if w3.is_connected():
             return w3
+
